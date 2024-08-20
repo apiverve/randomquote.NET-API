b9aa8bdd60e6b4b2a1fbad2e0b20236861b43c5d
APIVerve Team <hello@apiverve.com>
2024-08-20 11:05:09 -0500

    Bug Fixes

--- RandomQuoteAPIClient.cs
+++ RandomQuoteAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, randomquoteQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, RandomQuoteQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(randomquoteQueryOptions options = null)
+        public ResponseObj Execute(RandomQuoteQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(randomquoteQueryOptions options)
+        private string constructURL(RandomQuoteQueryOptions options)
         {
             string url = _apiEndpoint;
 
